ba5d7738ccead79fde51e31611bc786d77f3bce3
Sofia Alabart <sofia9303@gmail.com>
2025-07-17 14:41:19 +0200

    adding files

---
+++ scraper-python.py
@@ -0,0 +1,21 @@
+# Import libraries
+
+import requests
+from bs4 import BeautifulSoup
+
+def scrape():
+    url = 'https://www.example.com'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.select_one('h1').text
+    text = soup.select_one('p').text
+    link = soup.select_one('a').get('href')
+
+    print(title)
+    print(text)
+    print(link)
+
+if __name__ == '__main__':
+    scrape()
+
+
